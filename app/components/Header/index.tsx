b5e17291d1aece44be15932ad4a0f87f3abeb227
Juan Cardona <juanscr24@hotmail.com>
2023-09-25 17:18:35 -0500

    adding Section one

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -14,11 +14,11 @@ const Header = () => {
                     <a href="#">Contactos</a>
                 </div>
             </div>
-            <div className='grid grid-cols-2 ml-16 items-center max-sm:m-4 max-sm:grid-cols-1'>
+            <div className='grid grid-cols-2 ml-16 items-center max-sm:m-4 max-sm:grid-cols-1 max-xl:pb-5'>
                 <div className='flex flex-col gap-4 mr-16 max-sm:items-center max-sm:text-center max-sm:mr-2 max-sm:gap-1'>
                     <h1 className='text-3xl font-bold max-sm:text-base'>Hola! <br /> Soy Juan Cardona</h1>
                     <h2 className='text-cyan-700 text-xl font-bold'>Frontend Developer</h2>
-                    <p className='text-zinc-400 max-sm:text-sm'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit ab corrupti distinctio consequuntur tempora doloremque inventore culpa consequatur a beatae. Commodi, iusto assumenda! Ab sunt error, veniam porro corporis incidunt?</p>
+                    <p className='text-zinc-400 max-sm:text-sm'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit ab corrupti distinctio consequuntur tempora doloremque inventore culpa consequatur a beatae.</p>
                     <div className='flex gap-5 text-3xl max-sm:items-center max-sm:my-3'>
                         <a href=""><BsFacebook className='text-[#1877f2]' /></a>
                         <a href=""><BsLinkedin className='text-[#0a66c2]' /></a>
--- app/components/SectionOne/index.tsx
+++ app/components/SectionOne/index.tsx
@@ -3,7 +3,7 @@ import CardWork from '../CardWork'
 
 const SectionOne = () => {
     return (
-        <div className='w-3/4 bg-gray-900 flex flex-col items-center px-16 py-10 gap-2'>
+        <div className='w-3/4 bg-gray-900 flex flex-col items-center px-16 py-10 gap-2 max-lg:w-full'>
             <h1 className='text-white text-2xl font-bold'>Algunos de mis trabajos</h1>
             <p className='text-white'>Mis mejores trabajos</p>
             <div className='flex mt-5 flex-wrap justify-between'>
